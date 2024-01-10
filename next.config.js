b49cf53113bfd9fb9d2f03abd6ac52fdb588eb3d
Rossi Meacham <rossimeacham@gmail.com>
2024-01-10 18:27:08 -0500

    changes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.io",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "**.com",
+        port: "",
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig
